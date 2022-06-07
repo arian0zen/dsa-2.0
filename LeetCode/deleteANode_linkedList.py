f2f8d05a7d2e70fbb6a0c79a5fb0e1ef998a7abc
Arian Shaikh <ariansk90@gmail.com>
2022-06-08 03:03:22 +0530

    Create deleteANode_linkedList.py

---
+++ LeetCode/deleteANode_linkedList.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        #node.val == None
+        #print(node.val)
+        #print(node.next.val)
+        print(node.next)
+        print(node.next.next)
+        #print(node.val)
+        node.val = node.next.val
+        #print(node.val)
+        print(node.next)
+        print(node.next.next)
+        node.next = node.next.next
+        
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
