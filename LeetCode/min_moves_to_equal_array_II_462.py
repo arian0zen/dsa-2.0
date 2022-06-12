78eb0a3f69d3d0ae81a49701d4904745755c1cbf
Arian Shaikh <ariansk90@gmail.com>
2022-06-12 21:50:46 +0530

    Create min_moves_to_equal_array_II_462.py

---
+++ LeetCode/min_moves_to_equal_array_II_462.py
@@ -0,0 +1,28 @@
+from array import array
+
+
+# n = input("Enter the length of the array: ")
+# array = []
+# for i in range(int (n)):
+#     elem = int(input("Enter the elements of the array: "))
+#     array.append(elem)
+
+
+array = [1,10,2,9]
+
+array.sort()
+#print(array)
+n = len(array)
+mid = n//2
+#print(array[mid])   
+count = 0
+for element in range(n):
+    
+    if array[element] <= array[mid]:
+        count += array[mid] - array[element]
+    
+    else:
+        count += array[element] - array[mid]
+    
+print(count)
+        
